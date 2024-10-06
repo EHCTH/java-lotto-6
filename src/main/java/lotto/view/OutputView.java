c28e66c1f929871525a718fe86daf436fcedeac2
EHCTH <lees90896@gmail.com>
2024-10-06 19:11:28 +0900

    featL outputview add

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,16 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.domain.Profit;
+import lotto.domain.cost.IBuyerCost;
+import lotto.winningcost.WinningPlace;
+
+public class OutputView {
+    public static void resultInfo(Profit profit, IBuyerCost buyerCost) {
+        System.out.println("당첨 통계\n" + "---");
+        System.out.print(WinningPlace.print());
+        System.out.printf("총 수익률은 %.1f%%입니다.", profit.getProfit(buyerCost));
+    }
+    public static void purchaseAmount(IBuyerCost buyerCost) {
+        System.out.println(buyerCost.getTotal()+"\n");
+    }
 }
